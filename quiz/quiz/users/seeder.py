d0fc74b79d2d93b93fcfc3914e6056aff4d6961f
Aleksandra Połacik <polacik.ola@gmail.com>
2024-04-11 09:39:22 +0200

    add seeder

--- quiz/quiz/users/seeder.py
+++ quiz/quiz/users/seeder.py
@@ -1,57 +1,16 @@
-# from django_seed import Seed
-# from quizzes.models import Quizzes, Question, Answer, Category
-# import random
-
-# def generate_quizzes(seeder, number):
-#     seeder.add_entity(Quizzes, number, {
-#         'title': lambda x: seeder.faker.sentence(nb_words=4),
-#         'category': lambda x: Category.objects.get_or_create(name='Default Category')[0],
-#     })
-
-# def generate_questions(seeder, number):
-#     seeder.add_entity(Question, number, {
-#         'quiz': lambda x: random.choice(Quizzes.objects.all()),
-#         'technique': lambda x: random.randint(0, 0),  # assuming only one type of technique for now
-#         'title': lambda x: seeder.faker.sentence(nb_words=6),
-#         'difficulty': lambda x: random.randint(0, 4),
-#         'is_active': True
-#     })
-
-# def generate_answers(seeder, number):
-#     seeder.add_entity(Answer, number, {
-#         'question': lambda x: random.choice(Question.objects.all()),
-#         'answer_text': lambda x: seeder.faker.sentence(nb_words=3),
-#         'is_right': lambda x: random.choice([True, False]),
-#     })
-
-# def generate_categories(seeder):
-#     seeder.add_entity(Category, 1, {
-#         'name': 'Default Category',
-#     })
-
-# def generate_data():
-#     seeder = Seed.seeder()
-#     generate_categories(seeder)
-#     generate_quizzes(seeder, 10)
-#     generate_questions(seeder, 50)
-#     generate_answers(seeder, 200)
-#     inserted_pks = seeder.execute()
-#     print(inserted_pks)
-
-
-# if __name__ == '__main__':
-#     generate_data()
-
 from django_seed import Seed
 from .models import Quizzes, Question, Answer, Category
 import random
 
+# Funkcja do generowania quizow
 def generate_quizzes(seeder, number):
     seeder.add_entity(Quizzes, number, {
         'title': lambda x: seeder.faker.sentence(nb_words=4),
         'category': lambda x: Category.objects.get_or_create(name='Default Category')[0],
     })
 
+
+# Funkcja do generowania pytan
 def generate_questions(seeder, number):
     seeder.add_entity(Question, number, {
         'quiz': lambda x: random.choice(Quizzes.objects.all()),
@@ -62,6 +21,7 @@ def generate_questions(seeder, number):
     })
 
 
+# Funkcja do generowania odpowiedzi
 def generate_answers(seeder, number):
     seeder.add_entity(Answer, number, {
         'question': lambda x: random.choice(Question.objects.all()),
@@ -69,11 +29,15 @@ def generate_answers(seeder, number):
         'is_right': lambda x: random.choice([True, False]),
     })
 
+
+# Funkcja do generowania kategori
 def generate_categories(seeder):
     seeder.add_entity(Category, 1, {
         'name': 'Default Category',
     })
 
+
+# Funckja główna do generowania danych
 def generate_data(number):
     seeder = Seed.seeder()
     generate_categories(seeder)
